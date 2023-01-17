6e6242750949817155236f0a9971203e713d6b7e
Okellothomas <thomasolal33@gmail.com>
2023-01-17 19:54:50 +0300

    add js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 /**
  * Start the implementations 
  */
+let count = 0
 
+var myCount = document.getElementById("counter")
+
+function add() {
+    myCount.innerHTML = count ++
+}
+
+function sub() {
+    myCount.innerHTML = count --
+}
