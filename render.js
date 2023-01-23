6271f01e67d67bb1006a85843b4af7b050a2bcb6
Okellothomas <thomasolal33@gmail.com>
2023-01-23 10:24:24 +0300

    add the render.js implementation

---
+++ render.js
@@ -0,0 +1,11 @@
+/**
+ * Implementation of the render.js
+ */
+
+let myName = "Okello Thomas"
+var greetings = "Hi there, my Name is"
+
+let myGreatings = myName + " " + greetings + "!"
+
+let theGreatings = document.getElementById("welcomeMessage").innerHTML = myGreatings
+
