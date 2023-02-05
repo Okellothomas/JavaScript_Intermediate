45298517ee0182dcc7a7db8636b45790d388518a
Okellothomas <thomasolal33@gmail.com>
2023-02-05 08:55:37 +0300

    add the array implementaiton

---
+++ object.js
@@ -0,0 +1,15 @@
+/**
+ * Objects implementation
+ */
+
+let castle = {
+    status: true,
+    cost: 910,
+    place: "London",
+    rooms: 8,
+    weather: ["cold", 8, "hot"]
+}
+
+console.log(castle.place)
+console.log(castle["cost"])
+console.log(castle.weather)
