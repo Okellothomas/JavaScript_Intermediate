88657f55fbad7e1900b0518911e4d3e73a5b8a80
Okellothomas <thomasolal33@gmail.com>
2023-02-05 14:21:17 +0300

    add the button click challange

--- extension.js
+++ extension.js
@@ -2,6 +2,26 @@
  * code implementation
  */
 
-function Save() {
+function Myave() {
     console.log("button Clicked")
 }
+
+/**
+ * Add the event listiner
+ */
+
+let mybutton = document.getElementById("save")
+
+mybutton.addEventListener("click", function () {
+    console.log("Just got clicked")
+})
+
+/**
+ * Event listner example
+ */
+
+let myOpen = document.getElementById("open")
+
+myOpen.addEventListener("click", function () {
+    console.log("I want to open box when clicked!")
+})
