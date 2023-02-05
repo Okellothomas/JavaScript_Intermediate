38dcbcaeb9aa3a15ba7e877db6705e46fdf24c70
Okellothomas <thomasolal33@gmail.com>
2023-02-05 22:49:52 +0300

    add more changes

--- extension.js
+++ extension.js
@@ -2,36 +2,41 @@
 /**
  * Add the event listiner
  */
-let leads = ["One", "www.google.com"]
+let leads = []
 let myOpen = document.getElementById("open")
 let mybutton = document.getElementById("save")
 const ulEl = document.getElementById("ul")
 
 mybutton.addEventListener("click", function () {
     leads.push(myOpen.value)
-    console.log(leads)
+    myOpen.value = ""
+    render()
 })
 
-// log out the leads
-let listItems = ""
 
-let i = 0
-do {
-    listItems += "<li> " + leads[i] + "</li>"
-    i++
-} while (i < leads.length)
+function render() {
+    // log out the leads
+    let listItems = ""
 
-ulEl.innerHTML = listItems
+    let i = 0
+    do {
+        listItems += "<li><a href='#'>" + leads[i] + "</a></li>"
+        i++
+    } while (i < leads.length)
+
+    ulEl.innerHTML = listItems
+}
+
 
 /**
  * Use the forloop and the other implementation
  */
 
-for (let i = 0; i < leads.length; i++){
-    const li = document.createElement("li")
-    li.textContent += leads[i]
-    ulEl.append(li)
-}
+// for (let i = 0; i < leads.length; i++){
+//     const li = document.createElement("li")
+//     li.textContent += leads[i]
+//     ulEl.append(li)
+// }
 
 // inner html challange
 
