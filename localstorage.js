2aad7033a6d172a0e308990f07f0d5dcd6952b4d
Okellothomas <thomasolal33@gmail.com>
2023-02-06 14:43:21 +0300

    more applications

--- localstorage.js
+++ localstorage.js
@@ -5,3 +5,11 @@
 localStorage.setItem("leads", "www.jkuat.ac.ke")
 console.log(localStorage.getItem("leads"))
 localStorage.clear()
+
+// using json stringfy
+
+// let theleads = `["okello thomas]`
+// theleads = JSON.parse(theleads)
+// leads.push("Okello come home")
+// theleads = JSON.stringify(theleads)
+// console.log(teleads)
