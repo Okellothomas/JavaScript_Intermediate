4e2f13a8c05e6009ef294d210027939d1f3e83cf
Okellothomas <thomasolal33@gmail.com>
2023-02-06 16:41:38 +0300

    add more changes

--- extension.js
+++ extension.js
@@ -27,6 +27,12 @@ const tabs = [
 ]
 
 myUrl.addEventListener("click", function () {
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id
+    })
+
     leads.push(tabs[0].url)
     // console.log(tabs[0].url)
     localStorage.setItem("myLeads", JSON.stringify(leads))
