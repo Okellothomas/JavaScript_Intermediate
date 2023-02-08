8f798c9976e819dccba51ec43f61556dd775131f
Okellothomas <thomasolal33@gmail.com>
2023-02-08 10:09:37 +0300

    more react challange

--- react.js
+++ react.js
@@ -2,4 +2,10 @@
  * first react code
  */
 
-ReactDOM.render(<p>hello there</p>, document.getElementById("root"))
+ReactDOM.render(<>
+    <p>Okello should have come early</p>
+    <ol>
+        <li> This is the proper way of doing things</li>
+        <li> I think we should actually give him a chance!</li>
+    </ol>
+</>, document.getElementById("root"))
